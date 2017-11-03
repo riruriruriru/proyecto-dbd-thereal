9543a54596aa771a5d90f105d8becd80d4fa8f39
morris <smorris@live.cl>
2017-11-02 21:18:47 -0300

    factory final

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -40,7 +40,7 @@ $factory->define(App\Usuario::class, function (Faker $faker) {
 }
 
 
-$factory->define(App\Medida::class, function (Faker $faker) {
+$factory->define(App\Medidas::class, function (Faker $faker) {
 	$id_cat = \DB::table('Catastrofe')->select('id_catastrofe')->get();
 	$id_cat2 = $id_cat->random()->id;
 	$email = \DB::table('Usuario')->select('email')->get();
@@ -125,6 +125,127 @@ $factory->define(App\Trabajo::class, function (Faker $faker) {
         'id_tipo_trabajo' => $id_tipotrabtrab2,
         'id_rnv_trabajo' => $id_rnvtrab2,
         'tipo_trabajo' => str_random(30),
+    ];
+}
+
+
+
+$factory->define(App\Catastrofe::class, function (Faker $faker) {
+	$id_tipocatastrofe = \DB::table('TipoCatastrofe')->select('id_tipo_catastrofe')->get();
+	$id_tipocatastrofe2 = $id_tipocatastrofe->random()->id;
+
+    return [
+
+        'tipo_catastrofe' => $id_tipocatastrofe2,
+        'fecha_inicio' => $faker->dateTime,
+        'fecha_termino' => $faker->dateTime,
+    ];
+}
+
+
+
+$factory->define(App\Actividad::class, function (Faker $faker) {
+	$actividadidevento = \DB::table('Evento')->select('id_evento')->get();
+	$actividadidevento2 = $actividadidevento->random()->id;
+
+    return [
+
+        'actividad_id_evento' => $actividadidevento2,
+        'tipo' => str_random(100),
+    ];
+}
+
+
+
+$factory->define(App\Donacion::class, function (Faker $faker) {
+	$id_medon = \DB::table('Medidas')->select('id_medidas')->get();
+	$id_medon2 = $id_medon->random()->id;
+
+    return [
+
+    	'id_medidas_donacion' => $id_medon2,
+        'objetivo' => $faker->rand(1,50);
+        'numero_cuenta' => $faker->rand(1,50);
+    ];
+}
+
+
+$factory->define(App\Evento::class, function (Faker $faker) {
+	$id_medeven = \DB::table('Medidas')->select('id_medidas')->get();
+	$id_medeven2 = $id_medeven->random()->id;
+	
+    return [
+
+    	'fecha_inicio_evento' => $faker->dateTime,
+    	'fecha_termino_evento' => $faker->dateTime,
+    	'cantidad_voluntarios' => $faker->rand(1,100),
+    	'monto_recaudado' => $faker->rand(1,1000),
+    	'id_medidas_evento' => $id_medeven2,
+    ];
+}
+
+
+
+$factory->define(App\Donantes::class, function (Faker $faker) {
+	$id_donacion = \DB::table('Donacion')->select('id_donacion')->get();
+	$id_donacion2 = $id_donacion->random()->id;
+	$emaildonante = \DB::table('Usuario')->select('email')->get();
+	$emaildonante2 = $emaildonante->random()->id;
+	
+    return [
+
+    	'id_donacion_donante' => $id_donacion2,
+    	'email_donante' => $emaildonante2,
+    ];
+}
+
+
+
+$factory->define(App\Comentario::class, function (Faker $faker) {
+	$emailcoment = \DB::table('Usuario')->select('email')->get();
+	$emailcoment2 = $emailcoment->random()->id;
+	$id_comed = \DB::table('Medidas')->select('id_medidas')->get();
+	$id_comed2 = $id_comed->random()->id;
+	
+    return [
+
+    	'email' => $emailcoment2,
+    	'id_medidas' => $id_comed2,
+    	'descripcion' => $faker->text(),
+    ];
+}
+
+
+
+$factory->define(App\Voluntario::class, function (Faker $faker) {
+	$id_evolun = \DB::table('Evento')->select('id_evento')->get();
+	$id_evolun2 = $id_evolun->random()->id;
+	$id_volvol = \DB::table('Voluntariado')->select('id_voluntariado')->get();
+	$id_volvol2 = $id_volvol->random()->id;
+	$id_rnvol = \DB::table('RNV')->select('id_rnv')->get();
+	$id_rnvol2 = $id_rnvol->random()->id;
+	
+    return [
+
+    	'id_evento_voluntario' => $evolun2,
+    	'id_voluntariado_voluntario' => $id_volvol2,
+    	'id_rnv_voluntario' => $id_rnvol2,
+    	'nombre_voluntario' => $faker->str_random(20),
+    ];
+}
+
+
+
+
+$factory->define(App\Voluntariado::class, function (Faker $faker) {
+	$id_medvol = \DB::table('Medidas')->select('id_medidas')->get();
+	$id_medvol2 = $id_medvol->random()->id;
+	
+    return [
+
+    	'id_medidas_voluntariado' => $id_medvol2,
+    	'voluntarios_objetivo' => $faker->rand(1,50),
+    	'fecha_voluntariado' => $faker->dateTime,,
     ];
 }
 
