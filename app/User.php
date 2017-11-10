59f4a1e30e682298911673d8968521d58c6a7183
Unknown <isaac.espinoza@usach.cl>
2017-11-10 12:55:58 -0300

    Login UWU

--- app/User.php
+++ app/User.php
@@ -8,16 +8,14 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     use Notifiable;
-
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'last_name',
     ];
-
     /**
      * The attributes that should be hidden for arrays.
      *
