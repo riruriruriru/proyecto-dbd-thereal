b436424b054b9751155d29eec98b1a1cb9f38aeb
Isacc <isaac.espinoza@usach.cl>
2017-11-22 13:08:22 -0300

    modelo medidas

--- app/Medidas.php
+++ app/Medidas.php
@@ -7,8 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Medidas extends Model
 {
     //
-     protected $fillable = ['nombre', 'id_organizacion', 'fecha_inicio_medida', 'fecha_termino_medida', 'descripcion'];
+     protected $fillable = ['id_usuario', 'nombre_medida', 'id_organizacion_medidas', 'fecha_inicio_medida', 'fecha_termino_medida', 'descripcion', 'publico', 'id_historial', 'id_catastrofe_medidas'];
     protected $table = 'Medidas';
     public $timestamps = false;
     protected $primaryKey = 'id_medidas';
+
 }
