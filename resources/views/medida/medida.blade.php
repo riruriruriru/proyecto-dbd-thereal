5291964a14d4d80818086fa34e0fc8f944260417
Isacc <isaac.espinoza@usach.cl>
2017-11-25 13:56:51 -0300

    catastrofe lista

--- resources/views/medida/medida.blade.php
+++ resources/views/medida/medida.blade.php
@@ -200,7 +200,12 @@
   <div class="panel-body">
   <div class="row">
     <div class="col-md-7">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d3736489.7218514383!2d90.21589792292741!3d23.857125486636733!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1506502314230" width="100%" height="315" frameborder="0" style="border:0" allowfullscreen></iframe>
+        <iframe
+  width="450"
+  height="250"
+  frameborder="0" style="border:0"
+  src="https://www.google.com/maps/embed/v1/search?key=AIzaSyCVLEuprSyELEb_mOgivlT-hxuC5IbMVOk&q=record+stores+in+Seattle" allowfullscreen>
+</iframe>
       </div>
       <form class="form-horizontal" method="POST" action="{{ route('medida.store') }}" >
         {{ csrf_field() }}
@@ -219,9 +224,8 @@
 <!-- Text input-->
           <div class="col-md-6 form-line">
 
-          <div class="form-group">
-            <label for="textinput">Nombre Medida</label>  
-            <input id="id_catastrofe_medidas" name="id_catastrofe_medidas" placeholder="Nombre" class="form-control" required="" value={{$catastrofe}} type="text">
+          <div class="form-group"> 
+            <input id="id_catastrofe_medidas" name="id_catastrofe_medidas" placeholder="Nombre" class="form-control" required="" value={{$catastrofe}} type="hidden">
           </div>
 
           <!-- combox-->
@@ -258,7 +262,7 @@
       </div>
           </form>
         
-
+<?php echo $lugar_catastrofe ?>
 </div>
 </div>
 </div>
