326b9761bac9eb72d9d0ebfe1f82c69df8369864
Edu <eduardo.bustamante@usach.cl>
2017-12-04 11:49:33 -0300

    TRIGERINO migrar el proyecto

---
+++ database/migrations/2017_12_04_134933_create_trigger.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTrigger extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+        public function up()
+
+    {
+
+        DB::unprepared(
+            'CREATE TRIGGER over_donation BEFORE UPDATE OF 
+            `monto_actual` ON `Donacion`
+            BEGIN
+                IF NEW.monto_actual> OLD.objetivo THEN
+                   INSERT INTO `Donacion` (`monto_actual`) VALUES (OLD.monto_actual);
+                END IF;
+            END');
+    }
+
+    public function down()
+
+    {
+
+        DB::unprepared('DROP TRIGGER `over_donation`');
+
+    }
+}
